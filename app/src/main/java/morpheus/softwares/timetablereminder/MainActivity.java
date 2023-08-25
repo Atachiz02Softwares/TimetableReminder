8553df04b09911e204abcc541fb787bf96595205
mopheshi <ndachimya@gmail.com>
2023-08-25 10:26:00 +0100

    Initial commit

---
+++ app/src/main/java/morpheus/softwares/timetablereminder/MainActivity.java
@@ -0,0 +1,14 @@
+package morpheus.softwares.timetablereminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
